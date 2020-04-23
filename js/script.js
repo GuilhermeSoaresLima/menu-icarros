4dcf0b89a7d6f1003a2e259dc158304c5c40a000
Guilherme Soares Lima <guilherme.soares@icarros.com.br>
2020-04-23 12:27:15 -0300

    Layout concluido pendente correcao de nome de classes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-$(document).ready(function() {
-  $(".blue-header").click(function() {
+$(document).ready(function () {
+  $(".blue-header").click(function () {
     $(".header").addClass("header-color--active");
   });
-  $(".transparent-header").click(function() {
+  $(".transparent-header").click(function () {
     $(".header").removeClass("header-color--active");
   });
+});
+
+$(document).ready(function () {
+  let toggle = $(".menu-icon").hasClass("active");
+
+  if (toggle) {
+    $(".--active").click(function () {
+      $(".first-line").removeClass("first-animation--active");
+      $(".second-line").removeClass("second-animation--active");
+      $(".third-line").removeClass("third-animation--active");
+    });
+  } else {
+    $(".menu-icon").click(function () {
+      $(".menu-icon").addClass("--active");
+      $(".first-line").addClass("first-animation--active");
+      $(".second-line").addClass("second-animation--active");
+      $(".third-line").addClass("third-animation--active");
+    });
+  }
 });
